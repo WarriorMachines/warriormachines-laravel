1b2d7b2dfe9c517231a2a9d7ce92660343030571
Austin Maddox <austin@maddoxbox.com>
2016-02-04 09:00:13 -0700

    Add Google Analytics tracking code

--- resources/views/apex.blade.php
+++ resources/views/apex.blade.php
@@ -6,6 +6,15 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Warrior Machines</title>
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        
+        ga('create', 'UA-73347217-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 <body>
 
