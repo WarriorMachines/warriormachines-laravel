edc060f6002bd4f2b8ce9fdab025f51a38af1ef7
AustinMaddox <austin@maddoxbox.com>
2016-02-04 00:39:31 -0700

    Fancy bootstrap apex page

--- resources/views/apex.blade.php
+++ resources/views/apex.blade.php
@@ -9,6 +9,145 @@
 </head>
 <body>
 
+<nav class="navbar navbar-full navbar-dark bg-inverse">
+    <div class="container">
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+            &#9776;
+        </button>
+        <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+            {{--<a class="navbar-brand" href="#">WM</a>--}}
+            <ul class="nav navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="forums">Forums</a>
+                </li>
+                <li class="nav-item disabled">
+                    <a class="nav-link">Gallery</a>
+                </li>
+                <li class="nav-item disabled">
+                    <a class="nav-link">About</a>
+                </li>
+                <li class="nav-item disabled">
+                    <a class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item pull-xs-right">
+                    <a href="forums/ucp.php?mode=login" class="btn btn-success" role="button">Log in</a>
+                </li>
+            </ul>
+            {{--<form class="form-inline pull-xs-right">--}}
+            {{--<input class="form-control" type="text" placeholder="Search">--}}
+            {{--<button class="btn btn-success-outline" type="submit">Search</button>--}}
+            {{--</form>--}}
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-3">Warrior Machines</h1>
+        <p class="lead">The only community dedicated to the warriors who protect us.</p>
+        <a href="forums/ucp.php?mode=register" class="btn btn-primary btn-lg" role="button">Join the club</a>
+    </div>
+</div><!-- /.jumbotron -->
+
+<div class="container p-b-2">
+
+    <div class="row">
+        <div class="col-sm-7">
+            <img src="http://i66.tinypic.com/1zh0dug.jpg" class="img-fluid img-rounded">
+        </div>
+        <div class="col-sm-5">
+            <p class="font-weight-bold text-uppercase text-muted p-b-2">Honoring our heros</p>
+            <h4>Home of the free, because of the brave.</h4>
+            <p class="font-weight-normal text-muted p-b-3">To our men and women in uniform...past present and future- God bless you...and thank you.</p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="font-weight-bold">We salute you</div>
+                    <div class="text-muted">For the freedoms that we all enjoy... Our thanks go to the young men and women serving in all branches of the military. <a
+                                href="forums">Learn more</a>.
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="font-weight-bold">Never forgotten</div>
+                    <div class="text-muted">To be a veteran one must know and determine one's price for freedom. <a href="forums">Learn
+                            more</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div><!-- /.container -->
+
+<div class="container-fluid bg-inverse p-y-3">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-7">
+                <p class="font-weight-bold text-uppercase text-muted p-b-2">Thank a fire fighter</p>
+                <h4>Firefighters save more than homes. They save hearts, memories, and dreams.</h4>
+                <p class="font-weight-normal text-muted p-b-3">Readiness, dedication, and service.</p>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="font-weight-bold">Heroes</div>
+                        <div class="text-muted">Greater love has no one than this, that one lay down his life for his friends. <a href="forums">Learn
+                                more</a>.
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="font-weight-bold">Courage</div>
+                        <div class="text-muted">For your strength and courage in so many ways, for saving lives every day. Our community salutes you. <a href="forums">Learn
+                                more</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <img src="http://www.dragbike.com/photogallerys/03gotham_nyc/Gotham009.jpg" class="img-fluid img-circle">
+            </div>
+        </div>
+
+    </div><!-- /.container -->
+</div><!-- /.container-fluid -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <img src="http://www.nleomf.org/assets/images/hbb/hbb-logo-2.png" class="center-block">
+        </div>
+    </div>
+</div><!-- /.container -->
+
+<div class="container-fluid bg-primary p-y-2">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-8 center-block">
+                <p class="text-xs-center">Today, in the United States, more than 900,000 law enforcement officers put their lives on the line for the safety and
+                    protection of others. They serve with valor and distinction – and with great success.</p>
+                <p class="text-xs-center">Federal statistics show that violent and property crime rates in the
+                    United States are at historic lows, thanks in large measure to the dedicated service of the men and women of law enforcement.</p>
+            </div>
+        </div>
+    </div><!-- /.container -->
+</div><!-- /.container-fluid -->
+
+<div class="container-fluid bg-inverse p-a-3">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-6">
+                Copyright {{ date('Y') }} WarriorMachines.com. All rights reserved.
+            </div>
+            <div class="col-sm-6 text-sm-right">
+                <span class="text-muted">Austin kicks ass</span>
+            </div>
+        </div>
+
+    </div><!-- /.container -->
+</div><!-- /.container-fluid -->
 
 <script src="{{ elixir('js/all.js') }}"></script>
 </body>
