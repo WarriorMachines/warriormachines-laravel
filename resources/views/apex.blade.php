02a416b589a1f035593f70477a941bee33dcb968
AustinMaddox <austin@maddoxbox.com>
2016-02-21 23:25:54 -0700

    Modify main forums to 4 cols wide

--- resources/views/apex.blade.php
+++ resources/views/apex.blade.php
@@ -57,7 +57,7 @@
         <h1>Forums</h1>
         <div class="row text-xs-center m-b-1">
             @foreach ($main_forums as $forum)
-                <div class="col-sm-4">
+                <div class="col-sm-3">
                     <div class="fa-border m-t-1">
                         <a href="./forums/viewforum.php?f={{ $forum->forum_id }}" class="link-black">
                             <div><i class="fa fa-5x fa-folder-open"></i></div>
